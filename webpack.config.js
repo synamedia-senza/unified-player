76f0242d4f079b244774d449d82da235385c9fb5
mhefetz <nhefetz@synamedia.com>
2024-09-04 11:37:09 +0300

    update entry point in webpack config to use index.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 
 module.exports = {
-    entry: ["./banner.js"],
+    entry: ["./index.js"],
     mode: "development",
     output: {
         path: path.resolve(__dirname, "dist"),

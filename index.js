9b93708c075b37cdfe41690ac818791135c1c1f4
Andrew Zamler-Carhart <andrewzc@synamedia.com>
2024-10-31 16:06:22 -0400

    App now calls UnifiedPlayer directly instead of using VideoManager wrapper. Added convenience functions from VideoManager to index.js.

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 import { init, uiReady } from "senza-sdk";
-import { videoManager } from "./videoManager.js";
+import { UnifiedPlayer } from "./unifiedPlayer.js";
 
 const TEST_VIDEO = "https://dash.akamaized.net/akamai/bbb_30fps/bbb_30fps.mpd";
+
+let unifiedPlayer;
 
 window.addEventListener("load", async () => {
   try {
     await init();
-    videoManager.init(video);
-    await videoManager.load(TEST_VIDEO);
-    videoManager.play();
+    unifiedPlayer = new UnifiedPlayer(video);
+    await unifiedPlayer.load(TEST_VIDEO);
+    unifiedPlayer.play();
     uiReady();
   } catch (error) {
     console.error(error);
@@ -17,11 +19,31 @@ window.addEventListener("load", async () => {
 
 document.addEventListener("keydown", async function (event) {
   switch (event.key) {
-    case "Enter": await videoManager.toggleLocalAndRemotePlayback(); break;
-    case "Escape": videoManager.playPause(); break;
-    case "ArrowLeft": videoManager.skip(-30); break;
-    case "ArrowRight": videoManager.skip(30); break;
+    case "Enter": await togglePlayback(); break;
+    case "Escape": playPause(); break;
+    case "ArrowLeft": skip(-30); break;
+    case "ArrowRight": skip(30); break;
     default: return;
   }
   event.preventDefault();
 });
+
+async function togglePlayback() {
+  if (unifiedPlayer.isInRemotePlayback) {
+    await unifiedPlayer.moveToLocalPlayback();
+  } else {
+    unifiedPlayer.moveToRemotePlayback();
+  }
+}
+
+function playPause() {
+  if (unifiedPlayer.paused) {
+    unifiedPlayer.play();
+  } else {
+    unifiedPlayer.pause();
+  }
+}
+
+function skip(seconds) {
+  unifiedPlayer.currentTime = unifiedPlayer.currentTime + seconds;
+}

e85ef4857f89431de3912026edfe9b7ab4d3af30
Andrew Zamler-Carhart <andrewzc@synamedia.com>
2025-01-23 18:29:37 -0500

    Audio and subtitle language selection demo

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 import { init, uiReady, lifecycle } from "senza-sdk";
 import { ShakaPlayer } from "./shakaPlayer.js";
 
-const TEST_VIDEO = "https://dash.akamaized.net/akamai/bbb_30fps/bbb_30fps.mpd";
+const TEST_VIDEO = "https://storage.googleapis.com/shaka-demo-assets/angel-one/dash.mpd";
 
 let player;
+
+let audioLangs = ['en'];
+let selectedAudioIndex = 0;
+let textLangs = ['en'];
+let selectedTextIndex = 0;
 
 window.addEventListener("load", async () => {
   try {
@@ -11,6 +16,20 @@ window.addEventListener("load", async () => {
     player = new ShakaPlayer(video);
     await player.load(TEST_VIDEO);
     await video.play();
+
+    audioLangs = player.getAudioLanguages();
+    textLangs = player.getTextLanguages();
+    selectedAudioIndex = audioLangs.indexOf('en');
+    selectedTextIndex = audioLangs.indexOf('en');
+
+    console.log("audio", audioLangs, selectedAudioIndex);
+    console.log("text", textLangs, selectedTextIndex);
+
+    player.setTextTrackVisibility(true);
+
+    console.log("remote audio", player.remotePlayer.getAudioTracks());
+    console.log("remote text", player.remotePlayer.getTextTracks());
+
     uiReady();
   } catch (error) {
     console.error(error);
@@ -20,9 +39,12 @@ window.addEventListener("load", async () => {
 document.addEventListener("keydown", async function (event) {
   switch (event.key) {
     case "Enter": await toggleBackground(); break;
-    case "Escape": await playPause(); break;
-    case "ArrowLeft": skip(-30); break;
-    case "ArrowRight": skip(30); break;
+    case "Escape": video.muted = !video.muted; break;
+    case "ArrowUp": changeAudioLang(-1); break;
+    case "ArrowDown": changeAudioLang(1); break;
+    case "ArrowLeft": changeTextLang(-1); break;
+    case "ArrowRight": changeTextLang(1); 
+    break;
     default: return;
   }
   event.preventDefault();
@@ -39,14 +61,20 @@ async function toggleBackground() {
   }
 }
 
-async function playPause() {
-  if (video.paused) {
-    await video.play();
-  } else {
-    await video.pause();
-  }
+function changeAudioLang(delta) {
+  selectedAudioIndex = (selectedAudioIndex + delta + audioLangs.length) % audioLangs.length;
+  player.selectAudioLanguage(audioLangs[selectedAudioIndex]);
+  udpateBanner();
 }
 
-function skip(seconds) {
-  video.currentTime = video.currentTime + seconds;
+function changeTextLang(delta) {
+  selectedTextIndex = (selectedTextIndex + delta + textLangs.length) % textLangs.length;
+  player.selectTextLanguage(textLangs[selectedTextIndex]);
+  udpateBanner();
+}
+
+function udpateBanner() {
+  banner.innerHTML =
+    `audio: ${audioLangs[selectedAudioIndex]}<br>` +
+    `text: ${textLangs[selectedTextIndex]}`
 }
--- shakaPlayer.js
+++ shakaPlayer.js
@@ -112,6 +112,31 @@ export class ShakaPlayer extends shaka.Player {
     }
   }
 
+  setTextTrackVisibility(isVisible) {
+    super.setTextTrackVisibility(isVisible);
+    remotePlayer.setTextTrackVisibility(isVisible);
+  }
+
+  selectAudioLanguage(language, role, channelsCount, safeMargin, codec, spatialAudio) {
+    console.log("set audio:", language);
+    super.selectAudioLanguage(language, role, channelsCount, safeMargin, codec, spatialAudio);
+    
+    let audioTracks = remotePlayer.getAudioTracks();
+    if (audioTracks.length) {
+      let track = audioTracks.find(t => t.lang == language);
+      if (track) {
+        console.log("set remote audio:", track.id);
+        remotePlayer.selectAudioTrack(track.id);
+      }
+    }
+  }
+
+  selectTextLanguage(language, role, forced) {
+    console.log("set text:", language);
+    super.selectTextLanguage(language, role, forced);
+    remotePlayer.selectTextTrack(language);
+  }
+
   /**
    * Configures DRM settings.
    *
